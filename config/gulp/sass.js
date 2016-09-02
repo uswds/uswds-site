360135c48587d47de3a8798eb9e81228c55d1eba
Yoz (Jeremy) Grahame <jeremy.grahame@gsa.gov>
2016-09-02 11:23:50 -0700

    Move css, make build step copy it in (#13)

--- config/gulp/sass.js
+++ config/gulp/sass.js
@@ -3,6 +3,17 @@ var dutil     = require('./doc-util');
 var linter    = require('gulp-scss-lint');
 var task      = /([\w\d-_]+)\.js$/.exec(__filename)[ 1 ];
 
+gulp.task('copy-doc-styles', function (done) {
+
+  dutil.logMessage('copy-doc-styles', 'Copying Sass files from css/');
+
+  var stream = gulp.src('./css/**/*')
+  .pipe(gulp.dest('assets/css/'));
+
+  return stream;
+
+});
+
 gulp.task('copy-uswds-styles', function (done) {
 
   dutil.logMessage('copy-uswds-styles', 'Copying Sass files from uswds');
@@ -31,7 +42,7 @@ gulp.task('scss-lint', function (done) {
 
 });
 
-gulp.task(task, [ 'copy-uswds-styles', 'scss-lint' ], function (done) {
+gulp.task(task, [ 'copy-doc-styles', 'copy-uswds-styles', 'scss-lint' ], function (done) {
 
   dutil.logMessage(task, 'Compiling Sass');
 

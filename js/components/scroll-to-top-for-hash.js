c3a2ae8d0919afa150ce52a10390961727d4c76e
Shawn Allen <shawn.allen@gsa.gov>
2016-12-06 12:36:36 -0800

    isolate scroll-to-top-for-hash

--- js/components/scroll-to-top-for-hash.js
+++ js/components/scroll-to-top-for-hash.js
@@ -1,21 +1,32 @@
+'use strict';
+
 var $ = require('jquery');
+
 var calculateAnchorPosition = require('./calculate-anchor-position');
+var timeout;
 
 /* When user lands on a page with a hash in the url
  * default behavior will put the title at the very top
  * and the header will cover the top of the section.
  * This interrupts that and positions section title correctly
  */
-$(function () {
+function scrollToTopForHash () {
   var hash          = window.location.hash.substr(1);
   var scrollTopPos  = (hash ? calculateAnchorPosition(hash) : 0);
 
   if (scrollTopPos > 0) {
+    clearTimeout(timeout);
     //setTimeout ensures proper ordering of events
     //and makes this happens after the browser's default jump
-    setTimeout(function () {
+    timeout = setTimeout(function () {
       $(window).scrollTop(scrollTopPos);
     }, 1);
   }
+}
+
+// do it once on load
+$(function () {
+  // then do it every
+  setTimeout(scrollToTopForHash, 150);
 });
 

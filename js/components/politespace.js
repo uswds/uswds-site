d1ac00d53532b9573550de46e813ad76a6a52ef4
Shawn Allen <shawn.allen@gsa.gov>
2016-12-07 13:57:07 -0800

    use Politespace's init script w/enhance event

--- js/components/politespace.js
+++ js/components/politespace.js
@@ -1,43 +1,16 @@
+'use strict';
 var $ = require('jquery');
 
+// for Politespace
 window.jQuery = $;
 
-// README: This is necessary because politespace doesn't properly
-// export anything in its package.json.
-// TODO: Let's open a PR related to this so we can fix it there.
-var Politespace = require('politespace/src/politespace').Politespace;
+window.Politespace = require('politespace/src/politespace').Politespace;
+require('politespace/src/politespace-init');
 
-var componentName = 'politespace';
-var enhancedAttr = 'data-enhanced';
-var initSelector = '[data-' + componentName + ']:not([' + enhancedAttr + '])';
-
-$.fn[ componentName ] = function (){
-  return this.each(function (){
-    var polite = new Politespace(this);
-    if(polite.type === 'number') {
-      polite.createProxy();
-    }
-
-    $(this)
-      .bind('input keydown', function () {
-        polite.updateProxy();
-      })
-      .bind('blur', function () {
-        $(this).closest('.politespace-proxy').addClass('active');
-        polite.update();
-        polite.updateProxy();
-      })
-      .bind('focus', function () {
-        $(this).closest('.politespace-proxy').removeClass('active');
-        polite.reset();
-      })
-      .data(componentName, polite);
-
-    polite.update();
-  });
-};
-
-// auto-init on enhance (which is called on domready)
-$(function () {
-  $('[data-' + componentName + ']').politespace();
+/**
+ * XXX Politespace waits for an "enhance" event to fire to do its
+ * initialization. We do it for Politespace.
+ */
+$(function autoEnhance () {
+  $(document).trigger('enhance');
 });

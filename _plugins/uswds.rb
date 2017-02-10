059bc2b9e9cdb5591b71c6a4f863e3b8694fbccf
Shawn Allen <shawn.allen@gsa.gov>
2017-02-10 15:38:41 -0800

    add resolve_permalink template filter

--- _plugins/uswds.rb
+++ _plugins/uswds.rb
@@ -6,6 +6,19 @@ module USWDS
     )
   end
 
+  def resolve_permalink(href, source=nil)
+    if source.is_a?(Array)
+      pages = source
+    elsif source.is_a?(String)
+      pages = @context.registers[:site][source.to_sym]
+    else
+      pages = @context.registers[:site].pages
+    end
+    pages
+      .select { |page| page.permalink == href }
+      .first
+  end
+
 end
 
 Liquid::Template.register_filter(USWDS)

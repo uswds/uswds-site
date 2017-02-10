17b0f4aa163d44a6ac5516f86f3be03ed4ead738
Julia Elman <julia.elman@gsa.gov>
2017-02-10 13:43:53 -0500

    Initialize sticky fill JS.

--- js/start.js
+++ js/start.js
@@ -5,3 +5,11 @@ require('./vendor/stickyfill.min.js');
 require('./scroll-to-top-for-hash');
 require('./search');
 require('./sidenav');
+
+
+// Initialize sticky fill
+var stickyElements = document.getElementsByClassName('sticky');
+
+for (var i = stickyElements.length - 1; i >= 0; i--) {
+  Stickyfill.add(stickyElements[ i ]);
+}

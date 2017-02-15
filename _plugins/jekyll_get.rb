93008b399086595960d623aefcda168bd1ad6cb2
Julia Elman <julia.elman@gsa.gov>
2017-02-15 12:10:21 -0500

    Add check and output to decode base64 - thx @pkarman!

--- _plugins/jekyll_get.rb
+++ _plugins/jekyll_get.rb
@@ -1,6 +1,7 @@
 require 'json'
 require 'hash-joiner'
 require 'open-uri'
+require 'base64'
 
 module Jekyll_Get
   class Generator < Jekyll::Generator
@@ -16,19 +17,27 @@ module Jekyll_Get
         config = [config]
       end
       config.each do |d|
+        name_of_target = d['data']
         begin
-          target = site.data[d['data']]
+          target = site.data[name_of_target]
           source = JSON.load(open(d['json']))
           if target
             HashJoiner.deep_merge target, source
           else
-            site.data[d['data']] = source
+            site.data[name_of_target] = source
+          end
+          encoding = site.data[name_of_target]['encoding']
+          target_content = site.data[name_of_target]['content']
+          if encoding == 'base64'
+            site.data[name_of_target]['decoded'] = Base64.decode64(target_content).force_encoding(Encoding::UTF_8)
+          else
+            site.data[name_of_target]['decoded'] = target_content
           end
           if d['cache']
             data_source = (site.config['data_source'] || '_data')
-            path = "#{data_source}/#{d['data']}.json"
+            path = "#{data_source}/#{name_of_target}.json"
             open(path, 'wb') do |file|
-              file << JSON.generate(site.data[d['data']])
+              file << JSON.generate(site.data[name_of_target])
             end
           end
         rescue

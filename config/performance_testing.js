4de548671c7e8d5486c6f66b0c50bbf9717e282d
Marco <msegreto@miceover.com>
2017-03-16 19:53:08 -0700

    Adding script to run chrome lighthouse
    Starts the server, monitors for when the server start command happens
    and then runs google chrome lighthouse.

--- config/performance_testing.js
+++ config/performance_testing.js
@@ -1,66 +1,27 @@
 
-const exec = require('child_process').exec;
 const spawn = require('child_process').spawn;
 
 const Lighthouse = require('lighthouse');
 const ChromeLauncher = require('lighthouse/lighthouse-cli/chrome-launcher.js').ChromeLauncher;
 const Printer = require('lighthouse/lighthouse-cli/printer');
 
-const SITE_PORT = 8000;
-let process;
+function startServer() {
+  const process = spawn('bundle', ['exec', 'jekyll', 'serve']);
 
-process = spawn('bundle', ['exec', 'jekyll', 'serve']);
-
-process.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
-
-process.stdout.on('close', (data) => {
-  console.log('Close');
-});
-
-process.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`);
-});
-
-process.kill('SIGHUP');
-
-/*
-function execPromise(cmd, opts) {
-  opts || (opts = {});
-  return new Promise((resolve, reject) => {
-    process = exec(cmd, opts,
-      (err, stdout, stderr) => err ? reject(err) : resolve({
-        stdout: stdout,
-        stderr: stderr
-      }));
-
-    if (opts.stdout) {
-      process.stdout.pipe(opts.stdout());
-    }
-    if (opts.stderr) {
-      process.stderr.pipe(opts.stderr());
-    }
+  process.stdout.on('close', (data) => {
+    console.log('Server stopping');
   });
+
+  process.stderr.on('data', (data) => {
+    console.log(`ERROR: ${data}`);
+  });
+
+  return process;
 }
 
-function runSite() {
-  const run = execPromise('bundle exec jekyll serve', {
-    stdout: (data) => {
-      console.log(`server stdout: ${data}`);
-    },
-    stderr: (data) => {
-      console.log(`Running server returned an error, ${err}`);
-    }
-  })
-
-  return run;
-}
-
-function stopSite() {
+function stopServer(process) {
   process.kill('SIGHUP');
 }
-
 
 function launchChromeAndRunLighthouse(url, flags, config) {
   const launcher = new ChromeLauncher({port: 9222, autoSelectChrome: true});
@@ -82,22 +43,33 @@ function launchChromeAndRunLighthouse(url, flags, config) {
     });
 }
 
-// Use an existing config or create a custom one.
+const port = 4000;
 const config = require('lighthouse/lighthouse-core/config/perf.json');
-const url = 'https://example.com';
+const url = `localhost:${port}`;
 const flags = {output: 'html'};
 
 function main() {
-  runSite().then((run) => {
-    launchChromeAndRunLighthouse(url, flags, config).then(lighthouseResults => {
-      lighthouseResults.artifacts = undefined; // You can save the artifacts separately if so desired
-      return Printer.write(lighthouseResults, flags.output);
-    }).catch(err => { throw err; });
-  }).catch((err) => {
-    console.log(`ERROR: ${err}`);
-    throw err;
+  const server = startServer();
+
+  server.stdout.on('data', (data) => {
+    const stringData = `${data}`;
+    console.log(`SRV: ${stringData}`);
+
+    if (stringData.match(/Server address: http:/i)) {
+      launchChromeAndRunLighthouse(url, flags, config)
+      .then(lighthouseResults => {
+        lighthouseResults.artifacts = undefined; // You can save the artifacts separately if so desired
+        return Printer.write(lighthouseResults, flags.output);
+      })
+      .then(() => {
+        stopServer(server);
+      })
+      .catch(err => {
+        stopServer(server);
+        console.error(err);
+      });
+    }
   });
 }
 
 main();
-*/

815e675790c9d79eba2548a3679bc5ec09d261e7
Atul Varma <varmaa@gmail.com>
2017-07-03 18:01:08 -0400

    Support GitHub access tokens. Fixes #272.

--- _plugins/jekyll_get.rb
+++ _plugins/jekyll_get.rb
@@ -8,6 +8,26 @@ module Jekyll_Get
     safe true
     priority :highest
 
+    def decode_content(source)
+      encoding = source['encoding']
+      target_content = source['content']
+      if encoding == 'base64'
+        source['decoded'] = Base64.decode64(target_content).force_encoding(Encoding::UTF_8)
+      else
+        source['decoded'] = target_content
+      end
+    end
+
+    def get_final_url(url)
+      if url.start_with? "https://api.github.com/"
+        access_token = ENV['GITHUB_ACCESS_TOKEN']
+        if access_token
+          return "#{url}?access_token=#{access_token}"
+        end
+      end
+      url
+    end
+
     def generate(site)
       config = site.config['jekyll_get']
       if !config
@@ -18,20 +38,17 @@ module Jekyll_Get
       end
       config.each do |d|
         name_of_target = d['data']
+        url = d['json']
         begin
           target = site.data[name_of_target]
-          source = JSON.load(open(d['json']))
+          source = JSON.load(open(get_final_url(url)))
           if target
             HashJoiner.deep_merge target, source
           else
             site.data[name_of_target] = source
           end
-          encoding = site.data[name_of_target]['encoding']
-          target_content = site.data[name_of_target]['content']
-          if encoding == 'base64'
-            site.data[name_of_target]['decoded'] = Base64.decode64(target_content).force_encoding(Encoding::UTF_8)
-          else
-            site.data[name_of_target]['decoded'] = target_content
+          if d['decode_content']
+            decode_content site.data[name_of_target]
           end
           if d['cache']
             data_source = (site.config['data_source'] || '_data')
@@ -40,7 +57,8 @@ module Jekyll_Get
               file << JSON.generate(site.data[name_of_target])
             end
           end
-        rescue
+        rescue => e
+          print "jekyll_get: error fetching #{url}: #{e}\n"
           next
         end
       end

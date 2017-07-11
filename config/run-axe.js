c863e0d48b26ce5ad7e9b5c4954df1f1c1733b0f
Atul Varma <varmaa@gmail.com>
2017-07-11 16:53:57 -0400

    Run aXe on multiple pages.

--- config/run-axe.js
+++ config/run-axe.js
@@ -8,6 +8,11 @@ const runServer = require('./static-server');
 
 const REMOTE_CHROME_URL = process.env['REMOTE_CHROME_URL'];
 const AXE_JS = fs.readFileSync(__dirname + '/../node_modules/axe-core/axe.js');
+const PAGES = [
+  '/',
+  '/page-templates/landing/',
+  '/page-templates/docs/',
+];
 
 function launchChromeLocally(headless=true) {
   return chromeLauncher.launch({
@@ -48,15 +53,26 @@ let getChrome = REMOTE_CHROME_URL ? getRemoteChrome : launchChromeLocally;
 
 Promise.all([runServer(), getChrome()]).then(([server, chrome]) => {
   const chromeHost = chrome.host || 'localhost';
+  console.log(`Static file server is listening at ${server.url}.`);
   console.log(`Chrome is debuggable on http://${chromeHost}:${chrome.port}.`);
 
   CDP({
     host: chrome.host,
     port: chrome.port,
   }, client => {
-    console.log('Created CDP.');
-
     const {Page, Network, Runtime} = client;
+    const pagesLeft = PAGES.slice();
+    const loadNextPage = () => {
+      if (pagesLeft.length === 0) {
+        console.log(`Finished visiting ${PAGES.length} pages with no errors.`);
+        terminate(0);
+      } else {
+        const page = pagesLeft.pop();
+        const url = `${server.url}${page}`;
+        console.log(`Navigating to ${page}.`);
+        Page.navigate({url});
+      }
+    };
     const terminate = exitCode => {
       client.close().then(() => {
         chrome.kill().then(() => {
@@ -90,32 +106,38 @@ Promise.all([runServer(), getChrome()]).then(([server, chrome]) => {
         terminate(1);
       });
       Page.loadEventFired(() => {
-        console.log('Page loaded, running aXe.');
         Runtime.evaluate({
           expression: AXE_JS + ';(' + runAxe.toString() + ')()',
           awaitPromise: true,
         }).then(details => {
-          let exitCode = 0;
+          let errorFound = false;
           if (details.result.type === 'string') {
             const violations = JSON.parse(details.result.value);
-            if (violations.length === 0) {
-              console.log('No aXe violations found. Hooray!');
-            } else {
+            if (violations.length > 0) {
               console.log(`Found ${violations.length} aXe violations. Alas.`);
               console.log(violations);
-              exitCode = 1;
+              errorFound = true;
             }
           } else {
             console.log('Unexpected result from CDP!');
             console.log(details.result);
-            exitCode = 1;
+            errorFound = true;
           }
-          terminate(exitCode);
+          // Note that this means we're terminating on the first error we
+          // find, rather than reporting the error and moving on to the
+          // next page. We're doing this because it's possible that the
+          // error is caused by a layout or include, which may result
+          // in the same errors being reported across multiple pages, so
+          // we'd rather avoid the potential spam and just exit early.
+          if (errorFound) {
+            terminate(1);
+          } else {
+            loadNextPage();
+          }
         });
       });
 
-      console.log(`Navigating to ${server.url}.`);
-      Page.navigate({url: server.url});
+      loadNextPage();
     });
   });
 });

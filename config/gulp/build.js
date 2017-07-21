8ef93d8e8f32a87018e52880ee76a667b0f04246
Atul Varma <varmaa@gmail.com>
2017-07-21 08:28:44 -0400

    Add 'build-uswds-if-needed' gulp task.

--- config/gulp/build.js
+++ config/gulp/build.js
@@ -1,3 +1,6 @@
+var fs            = require('fs');
+var path          = require('path');
+var child_process = require('child_process');
 var gulp          = require('gulp');
 var dutil         = require('./doc-util');
 var runSequence   = require('run-sequence');
@@ -38,6 +41,37 @@ gulp.task('clean-assets', function (done) {
   );
 });
 
+// We might be using USWDS from a git repository instead of npm, in
+// which case it might not have the pre-built assets we need. If that's
+// the case, we'll want to build those assets.
+gulp.task('build-uswds-if-needed', function () {
+  const rootDir = path.normalize(path.join(__dirname, '..', '..'));
+  const uswdsDir = path.join(rootDir, 'node_modules', 'uswds');
+  const fractalIndex = path.join(uswdsDir, 'build', 'index.html');
+
+  if (fs.existsSync(fractalIndex)) {
+    dutil.logMessage('build-uswds-if-needed', 'USWDS is already built.');
+    return Promise.resolve();
+  } else {
+    dutil.logMessage('build-uswds-if-needed', 'Building USWDS...');
+    return new Promise((resolve, reject) => {
+      child_process.spawn(
+          'npm',
+          [ 'run', 'federalist' ],
+          { stdio: 'inherit', cwd: uswdsDir }
+        )
+        .on('error', reject)
+        .on('exit', code => {
+          if (code === 0) {
+            resolve();
+          } else {
+            reject(new Error("Building USWDS failed!"));
+          }
+        });
+    });
+  }
+});
+
 gulp.task('build', function (done) {
 
   dutil.logIntroduction();
@@ -47,6 +81,7 @@ gulp.task('build', function (done) {
 
   runSequence(
     'clean-assets',
+    'build-uswds-if-needed',
     [
       'fonts',
       'images',

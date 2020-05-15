7ffd89ed1bbee6e14560c94540c038346e2dade6
Dano Williams <daniel.williams@gsa.gov>
2020-05-15 10:26:26 -0700

    Move uswds-loader into an external script

--- config/gulp/javascript.js
+++ config/gulp/javascript.js
@@ -1,66 +1,81 @@
-const gulp        = require('gulp');
-const log       = require('fancy-log');
-const dutil       = require('./doc-util');
-const browserify  = require('browserify');
-const buffer      = require('vinyl-buffer');
-const source      = require('vinyl-source-stream');
-const uglify      = require('gulp-uglify');
-const sourcemaps  = require('gulp-sourcemaps');
-const rename      = require('gulp-rename');
-const linter      = require('gulp-eslint');
-const task        = 'javascript';
+const gulp = require("gulp");
+const log = require("fancy-log");
+const dutil = require("./doc-util");
+const browserify = require("browserify");
+const buffer = require("vinyl-buffer");
+const source = require("vinyl-source-stream");
+const uglify = require("gulp-uglify");
+const sourcemaps = require("gulp-sourcemaps");
+const rename = require("gulp-rename");
+const linter = require("gulp-eslint");
+const task = "javascript";
 
-gulp.task('eslint', function (done) {
-
+gulp.task("eslint", function (done) {
   if (!cFlags.test) {
-    dutil.logMessage('eslint', 'Skipping linting of JavaScript files.');
+    dutil.logMessage("eslint", "Skipping linting of JavaScript files.");
     return done();
   }
 
-  return gulp.src([
-    './js/**/*.js',
-    '!./js/vendor/**/*.js'])
-    .pipe(linter('.eslintrc'))
+  return gulp
+    .src(["./js/**/*.js", "!./js/vendor/**/*.js"])
+    .pipe(linter(".eslintrc"))
     .pipe(linter.format());
-
 });
 
-gulp.task('copy-uswds-javascript', function (done) {
+gulp.task("copy-uswds-javascript", function (done) {
+  dutil.logMessage(task, "Copying JS from uswds");
 
-  dutil.logMessage(task, 'Copying JS from uswds');
-
-  var stream = gulp.src('./node_modules/uswds/dist/js/**/*')
-    .pipe(gulp.dest('assets/js/vendor'));
+  var stream = gulp
+    .src("./node_modules/uswds/dist/js/**/*")
+    .pipe(gulp.dest("assets/js/vendor"));
 
   return stream;
-
 });
 
-gulp.task(task,
+gulp.task(
+  task,
   gulp.series(
-    gulp.parallel(
-      'copy-uswds-javascript',
-      'eslint'
-    ),
-    function(done) {
-      dutil.logMessage(task, 'Compiling JavaScript');
+    gulp.parallel("copy-uswds-javascript", "eslint"),
+    function (done) {
+      dutil.logMessage(task, "Compiling JavaScript");
 
       var minifiedStream = browserify({
-        entries: 'js/start.js',
+        entries: "js/start.js",
         debug: true,
       });
 
-      return minifiedStream.bundle()
-        .pipe(source('start.js'))
+      return minifiedStream
+        .bundle()
+        .pipe(source("start.js"))
         .pipe(buffer())
         .pipe(sourcemaps.init({ loadMaps: true }))
-          .pipe(uglify())
-          .on('error', log)
-          .pipe(rename({
-            basename: 'styleguide',
-          }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('assets/js'));
+        .pipe(uglify())
+        .on("error", log)
+        .pipe(
+          rename({
+            basename: "styleguide",
+          })
+        )
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("assets/js"));
+    },
+    function (done) {
+      dutil.logMessage(task, "Compiling Loader");
+
+      var minifiedStream = browserify({
+        entries: "js/uswds-loader.js",
+        debug: true,
+      });
+
+      return minifiedStream
+        .bundle()
+        .pipe(source("uswds-loader.js"))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(uglify())
+        .on("error", log)
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("assets/js"));
     }
   )
 );
---
+++ js/uswds-loader.js
@@ -0,0 +1,42 @@
+var loadJS = require("./vendor/loadjs.js");
+
+// EDITABLE: path to USWDS js
+var enhancedScriptPath = "../../assets/js/vendor/uswds.min.js";
+
+// Don't edit below this line ----------------------------------------------
+// Based on https://www.filamentgroup.com/lab/enhancing-optimistically.html
+if ("querySelector" in window.document && "addEventListener" in window) {
+  var docElem = window.document.documentElement;
+  var loadingClass = "usa-js-loading";
+  var loadedClass = "usa-js-present";
+  var defaultClass = "no-js";
+
+  function addLoadingClass() {
+    docElem.className = docElem.className.replace(defaultClass, loadingClass);
+  }
+
+  function removeDefaultClass() {
+    docElem.className = docElem.className.replace(defaultClass, "");
+  }
+
+  function revertClass() {
+    docElem.className = docElem.className.replace(loadingClass, defaultClass);
+  }
+
+  function switchToLoadedClass() {
+    docElem.className = docElem.className.replace(loadingClass, loadedClass);
+  }
+
+  addLoadingClass();
+
+  var script = loadJS(enhancedScriptPath);
+
+  var fallback = setTimeout(revertClass, 8000);
+
+  script.onload = function () {
+    clearTimeout(fallback);
+    removeDefaultClass();
+    // small timeout prevents FOUC
+    setTimeout(switchToLoadedClass, 100);
+  };
+}

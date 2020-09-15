b78bebbec407a3c63c3200f101f0d5b72fe0975f
Dano Williams <daniel.williams@gsa.gov>
2020-09-15 10:34:01 -0700

    Remove crawl shim

--- config/crawl.js
+++ config/crawl.js
@@ -22,9 +22,6 @@ function shouldFetch(item, referrerItem) {
     // almost guaranteed to be a false-positive that's actually
     // in an example snippet of HTML in the docs, so ignore it.
     return false;
-  } else if (item.path.match(/%20/)) {
-    // ignore paths that have a false < in them. a bug?
-    return false;
   } else if (item.path.match(/%3C/)) {
     // ignore paths that have a false < in them. a bug?
     return false;

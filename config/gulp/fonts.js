424b35edf7350ddf59d4d483d8dfc6674807fe13
Dano Williams <daniel.williams@gsa.gov>
2020-09-29 11:11:22 -0700

    Copy fonts from proper dist folder

--- config/gulp/fonts.js
+++ config/gulp/fonts.js
@@ -1,12 +1,12 @@
-var gulp  = require('gulp');
+var gulp = require('gulp');
 var dutil = require('./doc-util');
-var task  = 'fonts';
+var task = 'fonts';
 
 gulp.task(task, function (done) {
 
   dutil.logMessage(task, 'Copying fonts from uswds');
 
-  var stream = gulp.src('./node_modules/uswds/src/fonts/**/*')
+  var stream = gulp.src('./node_modules/uswds/dist/fonts/**/*')
     .pipe(gulp.dest('assets/fonts'));
 
   return stream;

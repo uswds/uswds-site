5ba8e42db321a000108ca0763c552ae711e6a22b
Dano Williams <daniel.williams@gsa.gov>
2020-12-03 15:39:42 -0800

    Fix eslint errors

--- js/uswds-loader.js
+++ js/uswds-loader.js
@@ -1,26 +1,26 @@
 // Based on https://www.filamentgroup.com/lab/enhancing-optimistically.html
 var docElem = window.document.documentElement;
-var loadingClass = "usa-js-loading";
-var loadedClass = "usa-js-present";
-var defaultClass = "no-js";
+var loadingClass = 'usa-js-loading';
+var loadedClass = 'usa-js-present';
+var defaultClass = 'no-js';
 
-function addLoadingClass() {
+function addLoadingClass () {
   docElem.className = docElem.className.replace(defaultClass, loadingClass);
 }
 
-function removeDefaultClass() {
-  docElem.className = docElem.className.replace(defaultClass, "");
+function removeDefaultClass () {
+  docElem.className = docElem.className.replace(defaultClass, '');
 }
 
-function revertClass() {
+function revertClass () {
   docElem.className = docElem.className.replace(loadingClass, defaultClass);
 }
 
-function switchToLoadedClass() {
+function switchToLoadedClass () {
   docElem.className = docElem.className.replace(loadingClass, loadedClass);
 }
 
-if ("querySelector" in window.document && "addEventListener" in window) {
+if ('querySelector' in window.document && 'addEventListener' in window) {
   addLoadingClass();
 
   var fallback = setTimeout(revertClass, 8000);
@@ -34,12 +34,12 @@ if ("querySelector" in window.document && "addEventListener" in window) {
         clearTimeout(fallback);
         removeDefaultClass();
         setTimeout(switchToLoadedClass, 100);
-        console.log("USWDS loaded.");
+        console.log('USWDS loaded.');
       } else if (timeout > 0) {
         poll();
-        console.log("Looking for USWDS...");
+        console.log('Looking for USWDS...');
       } else {
-        console.log("Library didn't load.");
+        console.log('Library didn\'t load.');
         // External library failed to load
       }
     }, 100);

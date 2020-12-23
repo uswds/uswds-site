86ebb569e2c66815dec109c3377f9f5e39b75f39
Jared Cunha <jared.cunha@gsa.gov>
2020-12-23 15:03:54 -0500

    move focus transfer to the smooth scrolling script

--- js/blueprint/internal-nav.js
+++ js/blueprint/internal-nav.js
@@ -3,7 +3,6 @@
 document.addEventListener('DOMContentLoaded', function (){
   
   var internalNav = document.getElementById('internal-nav');
-  var sectionBlock = document.getElementsByClassName('blueprint-section');
 
   if (typeof(internalNav) != 'undefined' && internalNav != null) {
 
@@ -30,31 +29,13 @@ document.addEventListener('DOMContentLoaded', function (){
       }
     });
 
-    // We need click events to handle keyboard navigation
+    // Click events
     for (var i = 0; i < internalNavLinks.length; i += 1) {
-      // Set tabindex = -1 so we can focus
-
-
-      // Now we focus on the section so the user can keyboard interact with that content
       internalNavLinks[ i ].addEventListener('click', function ( ) {
         var currentLink = this;
-        var currentSection = document.getElementById(this.getAttribute('href').replace('#', ''));
-        currentSection.setAttribute('tabindex', '-1');
-        
-        // Timeout prevents collisions with smooth scrolling.
-        setTimeout(function (){
-          currentSection.focus();
-          internalNavLinks.forEach(function (el){
-            el.classList.remove(CURRENT_CLASS);
-          });
-          currentLink.classList.add(CURRENT_CLASS);
-        }, 750);
+        this.classList.remove(CURRENT_CLASS);
+        currentLink.classList.add(CURRENT_CLASS);
       });
     }
-    
-    // Removes tabindex on blur to prevent click to focus sections
-    sectionBlock.addEventListener('blur', function () {
-      this.removeAttribute('tabindex');
-    });
   }
 });
--- js/blueprint/smooth-scroll.js
+++ js/blueprint/smooth-scroll.js
@@ -4,9 +4,22 @@ document.addEventListener('DOMContentLoaded', function (){
 
   anchorLink.forEach(function (elm) {
     elm.addEventListener('click', function (e) {
+      var anchorSection = document.getElementById(this.getAttribute('href').replace('#', ''));
+
       e.preventDefault();
       document.querySelector(this.getAttribute('href')).scrollIntoView({
         behavior: 'smooth',
+      });
+
+      anchorSection.setAttribute('tabindex', '-1');
+
+      // Timeout prevents collisions with smooth scrolling.
+      setTimeout(function (){
+        anchorSection.focus();
+      }, 750);
+      
+      anchorSection.addEventListener('blur', function () {
+        this.removeAttribute('tabindex');
       });
     });
   });

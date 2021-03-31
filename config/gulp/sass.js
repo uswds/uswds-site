04d0ba52f3bd305f3386bf7b1e1b363316e5c372
James Mejia <james.mejia@bixal.com>
2021-03-31 09:23:19 -0500

    Eslint: gulp sass.js fixes.

--- config/gulp/sass.js
+++ config/gulp/sass.js
@@ -1,133 +1,133 @@
-const autoprefixer = require("autoprefixer");
-const concat = require("gulp-concat");
-const csso = require("postcss-csso");
-const dutil = require("./doc-util");
-const gulp = require("gulp");
-const linter = require("gulp-scss-lint");
-const postcss = require("gulp-postcss");
-const sass = require("gulp-sass");
-const sourcemaps = require("gulp-sourcemaps");
-const task = "sass";
+const autoprefixer = require('autoprefixer');
+const concat = require('gulp-concat');
+const csso = require('postcss-csso');
+const dutil = require('./doc-util');
+const gulp = require('gulp');
+const linter = require('gulp-scss-lint');
+const postcss = require('gulp-postcss');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const task = 'sass';
 
-sass.compiler = require("sass");
+sass.compiler = require('sass');
 
 const dev_plugins = [autoprefixer({ cascade: false })];
 
 const prod_plugins = [csso({ forceMediaMerge: false })];
 
-gulp.task("build-sass-fonts", function() {
+gulp.task('build-sass-fonts', () => {
   return gulp
-    .src("./css/uswds-fonts.scss")
+    .src('./css/uswds-fonts.scss')
     .pipe(sourcemaps.init({ largeFile: true }))
     .pipe(
       sass
         .sync({
-          includePaths: ["./node_modules/uswds/dist/scss", "./css/settings"],
-          outputStyle: "expanded"
+          includePaths: ['./node_modules/uswds/dist/scss', './css/settings'],
+          outputStyle: 'expanded'
         })
-        .on("error", function(error) {
+        .on('error', (error) => {
           sass.logError.bind(this)(error);
 
-          if (process.env.NODE_ENV !== "development") {
+          if (process.env.NODE_ENV !== 'development') {
             process.exit(1);
           }
         })
     )
     .pipe(postcss(dev_plugins))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("assets/css"))
-    .pipe(gulp.dest("_site/assets/css"));
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('assets/css'))
+    .pipe(gulp.dest('_site/assets/css'));
 });
 
-gulp.task("build-sass-components", function() {
+gulp.task('build-sass-components', () => {
   return gulp
-    .src("./css/uswds-components.scss")
+    .src('./css/uswds-components.scss')
     .pipe(sourcemaps.init({ largeFile: true }))
     .pipe(
       sass
         .sync({
-          includePaths: ["./css/settings"],
-          outputStyle: "expanded"
+          includePaths: ['./css/settings'],
+          outputStyle: 'expanded'
         })
-        .on("error", function(error) {
+        .on('error', (error) => {
           sass.logError.bind(this)(error);
 
-          if (process.env.NODE_ENV !== "development") {
+          if (process.env.NODE_ENV !== 'development') {
             process.exit(1);
           }
         })
     )
     .pipe(postcss(dev_plugins))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("assets/css"))
-    .pipe(gulp.dest("_site/assets/css"));
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('assets/css'))
+    .pipe(gulp.dest('_site/assets/css'));
 });
 
-gulp.task("build-sass-custom", function() {
+gulp.task('build-sass-custom', () => {
   return gulp
-    .src("./css/uswds-custom.scss")
+    .src('./css/uswds-custom.scss')
     .pipe(sourcemaps.init({ largeFile: true }))
     .pipe(
       sass
         .sync({
-          includePaths: ["./node_modules/uswds/dist/scss", "./css/settings"],
-          outputStyle: "expanded"
+          includePaths: ['./node_modules/uswds/dist/scss', './css/settings'],
+          outputStyle: 'expanded'
         })
-        .on("error", function(error) {
+        .on('error', (error) => {
           sass.logError.bind(this)(error);
 
-          if (process.env.NODE_ENV !== "development") {
+          if (process.env.NODE_ENV !== 'development') {
             process.exit(1);
           }
         })
     )
     .pipe(postcss(dev_plugins))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("assets/css"))
-    .pipe(gulp.dest("_site/assets/css"));
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('assets/css'))
+    .pipe(gulp.dest('_site/assets/css'));
 });
 
-gulp.task("build-sass-utilities", function() {
+gulp.task('build-sass-utilities', () => {
   return gulp
-    .src("./css/uswds-utilities.scss")
+    .src('./css/uswds-utilities.scss')
     .pipe(sourcemaps.init({ largeFile: true }))
     .pipe(
       sass
         .sync({
-          includePaths: ["./node_modules/uswds/dist/scss", "./css/settings"],
-          outputStyle: "expanded"
+          includePaths: ['./node_modules/uswds/dist/scss', './css/settings'],
+          outputStyle: 'expanded'
         })
-        .on("error", function(error) {
+        .on('error', (error) => {
           sass.logError.bind(this)(error);
 
-          if (process.env.NODE_ENV !== "development") {
+          if (process.env.NODE_ENV !== 'development') {
             process.exit(1);
           }
         })
     )
     .pipe(postcss(dev_plugins))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("assets/css"))
-    .pipe(gulp.dest("_site/assets/css"));
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('assets/css'))
+    .pipe(gulp.dest('_site/assets/css'));
 });
 
 gulp.task(
-  "build-sass-dev",
+  'build-sass-dev',
   gulp.parallel(
-    "build-sass-fonts",
-    "build-sass-components",
-    "build-sass-custom",
-    "build-sass-utilities"
+    'build-sass-fonts',
+    'build-sass-components',
+    'build-sass-custom',
+    'build-sass-utilities'
   )
 );
 
-gulp.task("build-sass-prod", function() {
+gulp.task('build-sass-prod', () => {
   return gulp
     .src([
-      "./assets/css/uswds-fonts.css",
-      "./assets/css/uswds-components.css",
-      "./assets/css/uswds-custom.css",
-      "./assets/css/uswds-utilities.css"
+      './assets/css/uswds-fonts.css',
+      './assets/css/uswds-components.css',
+      './assets/css/uswds-custom.css',
+      './assets/css/uswds-utilities.css'
     ])
     .pipe(
       sourcemaps.init({
@@ -135,29 +135,29 @@ gulp.task("build-sass-prod", function() {
         loadMaps: true
       })
     )
-    .pipe(concat("styles.css"))
+    .pipe(concat('styles.css'))
     .pipe(postcss(prod_plugins))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("assets/css"))
-    .pipe(gulp.dest("_site/assets/css"));
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('assets/css'))
+    .pipe(gulp.dest('_site/assets/css'));
 });
 
-gulp.task("build-sass", gulp.series("build-sass-dev", "build-sass-prod"));
+gulp.task('build-sass', gulp.series('build-sass-dev', 'build-sass-prod'));
 
-gulp.task("scss-lint", function(done) {
+gulp.task('scss-lint', (done) => {
   if (!cFlags.test) {
-    dutil.logMessage("scss-lint", "Skipping linting of Sass files.");
+    dutil.logMessage('scss-lint', 'Skipping linting of Sass files.');
     return done();
   }
 
   return gulp
-    .src(["./css/**/*.scss"])
+    .src(['./css/**/*.scss'])
     .pipe(
       linter({
-        config: ".scss-lint.yml"
+        config: '.scss-lint.yml'
       })
     )
-    .pipe(linter.failReporter("E"));
+    .pipe(linter.failReporter('E'));
 });
 
-gulp.task(task, gulp.series("build-sass"));
+gulp.task(task, gulp.series('build-sass'));

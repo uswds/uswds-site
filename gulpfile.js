1620aeb294ae016ad52e92eb318a363fdcb51f17
James Mejia <james.mejia@bixal.com>
2022-05-23 08:39:07 -0500

    Add files to test USWDS Compile custom icons dir

--- gulpfile.js
+++ gulpfile.js
@@ -13,16 +13,24 @@ const dutil = require( './config/gulp/doc-util' );
 const uswds = require("@uswds/compile");
 
 uswds.settings.version = 3;
+uswds.settings.custom_sprites_only = true;
 
 uswds.paths.dist.theme = "./css";
 uswds.paths.dist.img = "./assets/img";
 uswds.paths.dist.fonts = "./assets/fonts";
 uswds.paths.dist.js = "./assets/js/vendor";
 uswds.paths.dist.css = "./assets/css";
+uswds.paths.dist.icons = "./img/test-usa-icons";
+
 
 /**
  * USWDS Compile tasks
  */
 exports.init = uswds.init;
+exports.watch = uswds.watch;
+
+exports.copyAssets = uswds.copyAssets;
+
 exports.compile = uswds.compile;
+exports.compileSass = uswds.compileSass;
 exports.compileIcons = uswds.compileIcons;

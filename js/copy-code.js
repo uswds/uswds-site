8336dce3e474604cdf462163933ad8252d7ef54b
Charlie Mahoney <charlie.mahoneyy@gmail.com>
2023-04-21 11:20:26 -0400

    Add sr-only text for additional context - Removes aria-live attribute because it was causing VoiceOver to stutter

--- js/copy-code.js
+++ js/copy-code.js
@@ -4,18 +4,20 @@ COPY_BUTTONS.forEach(button => {
   button.addEventListener("click", () => {
     // Set success state
     button.classList.add("usa-accordion__copy-button--success");
-    button.textContent = "Copied!";
-
+    button.querySelector("[aria-hidden]").textContent = "Copied!";
+    button.querySelector(".usa-sr-only").textContent = "Copied code to clipboard";
+    
     // After timeout, reset to default state
     setTimeout(() => {
       button.classList.remove("usa-accordion__copy-button--success");
-      button.textContent = "Copy";
+      button.querySelector("[aria-hidden]").textContent = "Copy";
+      button.querySelector(".usa-sr-only").textContent = "Copy component code";
     }, 3000);
 
     // Select section code and copy to clipboard
-    const CONTENT =  button.parentNode;
+    const WRAPPER =  button.parentNode;
+    const CONTENT = WRAPPER.parentNode;
     const CODE = CONTENT.querySelector("code");
     return navigator.clipboard.writeText(CODE.textContent);
   });
 });
-

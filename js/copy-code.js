4342599e25795ae9d6af0b51730ecf7830ad60e4
Charlie Mahoney <charlie.mahoneyy@gmail.com>
2023-05-25 13:49:51 -0400

    Update SR status message and relevant elements

--- js/copy-code.js
+++ js/copy-code.js
@@ -2,8 +2,14 @@ const COPY_CODE_CLASS = "copy-code";
 const COPY_BUTTON_CLASS = `usa-button usa-button--outline ${COPY_CODE_CLASS}__button`;
 const COPY_BUTTON_SUCCESS_CLASS = `${COPY_CODE_CLASS}__button--success`;
 const COPY_WRAPPER_CLASS = `${COPY_CODE_CLASS}__wrapper`;
+const SR_STATUS_MESSAGE_CLASS = `${COPY_CODE_CLASS}__sr-status`;
 
 const COPY_CODE = document.querySelectorAll(`.${COPY_CODE_CLASS}`);
+const COPY_WRAPPER = `.${COPY_WRAPPER_CLASS}`;
+const SR_STATUS_MESSAGE = `.${SR_STATUS_MESSAGE_CLASS}`;
+
+const SR_DEFAULT_MESSAGE = "Copy component code.";
+const SR_SUCCESS_MESSAGE = "Code copied to clipboard.";
 
 /**
  * Creates a wrapper for copy button.
@@ -25,13 +31,13 @@ const createWrapper = () => {
 const createCopyButton = () => {
   const btn = document.createElement("button");
   const btnText = `
-    <span class="" aria-hidden="true">Copy</span>
-    <span class="usa-sr-only">Copy component code</span>
+    <span aria-hidden="true">Copy</span>
   `;
 
   btn.className = COPY_BUTTON_CLASS;
   btn.setAttribute("type", "button");
-  btn.setAttribute("aria-live", "polite");
+  btn.setAttribute("aria-describedby", SR_STATUS_MESSAGE_CLASS);
+  ;
 
   btn.insertAdjacentHTML("beforeend", btnText);
 
@@ -44,10 +50,11 @@ const createCopyButton = () => {
  * @returns {HTMLElement} - A span for screen reader text
  */
 const createSRStatus = () => {
-  const srText = document.createElement("span");
+  const srText = document.createElement("div");
   srText.classList.add("usa-sr-only");
+  srText.classList.add(SR_STATUS_MESSAGE_CLASS);
   srText.setAttribute("aria-live", "polite");
-  srText.innerHTML = "Code copied to clipboard";
+  srText.innerHTML = SR_DEFAULT_MESSAGE;
 
   return srText;
 };
@@ -60,20 +67,21 @@ const createSRStatus = () => {
  */
 const copyOnClick = (event) => {
   const copyBtn = event.currentTarget;
-  const copyWrapper = copyBtn.closest(`.${COPY_WRAPPER_CLASS}`);
+  const btnWrapper = copyBtn.closest(COPY_WRAPPER);
   const labelVisual = copyBtn.querySelector("[aria-hidden]");
-  const labelSROnly = createSRStatus();
+  const labelSR = btnWrapper.querySelector(SR_STATUS_MESSAGE);
+
 
   // Set success state
   copyBtn.classList.add(COPY_BUTTON_SUCCESS_CLASS);
   labelVisual.innerHTML = "Copied!";
-  copyWrapper.appendChild(labelSROnly);
+  labelSR.innerHTML = SR_SUCCESS_MESSAGE;
 
   // After timeout, reset to default state
   setTimeout(() => {
     copyBtn.classList.remove(COPY_BUTTON_SUCCESS_CLASS);
-    labelVisual.textContent = "Copy";
-    labelSROnly.remove();
+    labelVisual.innerHTML = "Copy";
+    labelSR.innerHTML = SR_DEFAULT_MESSAGE;
   }, 3000);
 
   // Select section code and copy to clipboard
@@ -93,7 +101,9 @@ const init = () => {
   COPY_CODE.forEach((copyCodeElement) => {
     const copyWrapper = createWrapper();
     const copyButton = createCopyButton();
-
+    const labelSROnly = createSRStatus();
+    
+    copyWrapper.appendChild(labelSROnly);
     copyWrapper.appendChild(copyButton);
     copyCodeElement.appendChild(copyWrapper);
 
